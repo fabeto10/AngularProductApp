ef082e8ebe4a5b2df1156c4187eea97956d4ba0c
fabeto10 <f0414475@gmail.com>
2022-11-01 08:59:48 -0400

    agregando estilos y creacion de componente nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,13 @@ import {Product} from '../app/models/product.model'
 })
 export class AppComponent {
   imgParent = '';
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-  ];
+  showImg = true;
 
   onLoaded(img: string){
     console.log('Log Padre', img);
+  }
+
+  toggleImg(){
+    this.showImg = !this.showImg;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,63 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input() img: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  img: string = '';
+
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img => ', this.img);
+  }
+
+  @Input() alt: string = '';
+
   @Output() loaded = new EventEmitter<string>();
-  imageDefault = '../../../assets/default.png'
-  constructor() { }
+
+  imageDefault = '../../../assets/default.png';
+  // counter = 0;
+  // counterFn: number | undefined;
+  constructor() {
+    // before render
+    // No async -- once time
+    console.log("constructor", 'imgValue: ', this.img);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+      // before and during render
+      // changes input -- times
+      console.log('ngChanges', 'imgValue => ', this.img);
+      console.log('changes', changes);
+      // if(changes.) {
+      // // code
+      // };
+  }
 
   ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue => ', this.img);
+    // this.counterFn = window.setInterval(() => {
+    //   this.counter += 1;
+    //   console.log('run counter');
+    // }, 1000)
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+      // delete
+      console.log('ngOnDestroy');
+      // window.clearInterval(this.counterFn);
   }
   imgError() {
     this.img = this.imageDefault;
